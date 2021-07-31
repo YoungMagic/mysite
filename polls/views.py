807c5b040d854af336fae2ceb2c40681926284b2
YoungMagic <youngmagicp@163.com>
2021-07-31 23:04:30 +0800

    #comment 注释作业代码

--- polls/views.py
+++ polls/views.py
@@ -12,24 +12,24 @@ from django.views import generic
 from django.utils import timezone
 from django.core.cache import cache
 from django.http import HttpResponse
-from .models import Question, Choice
+# from .models import Question, Choice
 
 logger = logging.getLogger(__name__)
 
 
 # version 1
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    if request.method == 'GET':
-        question = get_object_or_404(Question, pk=question_id)
-        return render(request, 'polls/results.html', {'question': question})
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+#
+#
+# def results(request, question_id):
+#     if request.method == 'GET':
+#         question = get_object_or_404(Question, pk=question_id)
+#         return render(request, 'polls/results.html', {'question': question})
+#
+#
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
 
 
 
